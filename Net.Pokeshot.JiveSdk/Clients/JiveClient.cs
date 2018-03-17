643b31f6bda7093f63b8b14fa6ae235d826e9541
Eric Anastas <aireq303@gmail.com>
2018-03-17 02:53:36 -0700

    Added PostBinary to JiveClient to allow posting images

--- Net.Pokeshot.JiveSdk/Clients/JiveClient.cs
+++ Net.Pokeshot.JiveSdk/Clients/JiveClient.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Web;
 using Newtonsoft.Json.Linq;
 using Net.Pokeshot.JiveSdk.Models;
-
+using System.Net.Http.Headers;
 
 namespace Net.Pokeshot.JiveSdk.Clients
 {
@@ -223,6 +223,62 @@ namespace Net.Pokeshot.JiveSdk.Clients
             Interlocked.Increment(ref _numPosts);
             return cleanResponseActivities;
         }
+
+        protected string PostBinary(string url, byte[] data, string fileName)
+        {
+            HttpClientHandler jiveHandler = new HttpClientHandler();
+
+            //Setting credentials for our request. This needs to be done for every request as there are no persistent sessions for the REST Api
+            _credential.Domain = JiveCommunityUrl + "/api/core/v3";
+            //Getting our credentials in Base64 encoded format
+            string cre = String.Format("{0}:{1}", _credential.UserName, _credential.Password);
+            byte[] bytes = Encoding.UTF8.GetBytes(cre);
+            string base64 = Convert.ToBase64String(bytes);
+
+            //Set credentials and make sure we are pre-authenticating our request
+            jiveHandler.Credentials = _credential;
+            jiveHandler.PreAuthenticate = true;
+            jiveHandler.UseDefaultCredentials = true;
+
+            HttpClient httpClient = new HttpClient(jiveHandler);
+            //informs the HttpClient of the authorization type and media type
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64);
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, url);
+
+            if (_imposter != null)
+            {
+                requestMessage.Headers.Add("X-Jive-Run-As", "userid " + _imposter);
+            }
+
+            var requestContent = new MultipartFormDataContent();
+
+            var imgContent = new ByteArrayContent(data);
+            imgContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+
+            requestContent.Add(imgContent, "file", fileName);
+            requestMessage.Content = requestContent;
+
+            HttpResponseMessage activityResponse = httpClient.SendAsync(requestMessage).Result;
+
+            if (!activityResponse.IsSuccessStatusCode)
+            {
+                // Calling methods should handle this exception on a case by case basis.
+                // The exception contains the returned status code. Jive documentation describes what to do in the case of each code.
+                string message = "Jive Request Failed. Got response " + ((int)activityResponse.StatusCode).ToString() + " " + activityResponse.StatusCode +
+                    " when making POST request to " + url + "\nUsername: " + _credential.UserName + "\nPassword: " + _credential.Password;
+                throw new HttpException((int)activityResponse.StatusCode, message);
+            }
+
+            String myActivityResponse = activityResponse.Content.ReadAsStringAsync().Result;
+            //Remove the string Jive includes in every response from the REST API
+            string cleanResponseActivities = myActivityResponse.Replace("throw 'allowIllegalResourceCall is false.';", "");
+
+            Interlocked.Increment(ref _numPosts);
+            return cleanResponseActivities;
+        }
+
 
         //PutAbsolute takes its JSON content as a string, similar to the string returned by the GetAbsolute method
         protected string PutAbsolute(string url, string json)
