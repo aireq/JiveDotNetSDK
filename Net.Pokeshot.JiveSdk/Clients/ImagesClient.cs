da4f86c8fb30b404d310625c5759d88b97e15735
Eric Anastas <aireq303@gmail.com>
2018-03-17 00:52:19 -0700

    Added placeholder methods to ImagesClient

--- Net.Pokeshot.JiveSdk/Clients/ImagesClient.cs
+++ Net.Pokeshot.JiveSdk/Clients/ImagesClient.cs
@@ -4,11 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.IO;
+using Net.Pokeshot.JiveSdk.Models;
 
 namespace Net.Pokeshot.JiveSdk.Clients
 {
-    class ImagesClient : JiveClient
+    public class ImagesClient : JiveClient
     {
-        public ImagesClient(string communityUrl, NetworkCredential credentials) : base(communityUrl, credentials) { }
+        public ImagesClient(string communityUrl, NetworkCredential credentials) : base(communityUrl, credentials)
+        {
+        }
+
+        private string imagesUrl
+        { get { return JiveCommunityUrl + "/api/core/v3/images"; } }
+
+        public Net.Pokeshot.JiveSdk.Models.Image CreateImage(System.IO.Stream imageData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Image> GetContentImages(int contentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.IO.Stream GetCustomStatusLevelImage(string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.IO.Stream GetDefaultStatusLevelImage(string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.IO.Stream GetImage(int imageId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
